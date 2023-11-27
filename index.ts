f402fd786de62359ed81faf9aff2f6767e658917
imran khan <imranloveh01@gmail.com>
2023-11-27 18:14:37 +0500

    Fix console.log binding and add type alias for Person

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
-// use bind console.log to a variable
+// bind console.log to a variable
 const log = console.log.bind(console, "The answer is: ");
 
+// learning type alias in typescript
 type Person={
     name:string;
     age:number;
+}
+
+const person:Person = {
+    name:"John",
+    age:30
 }
