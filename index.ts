5fb6e1b1be188a05432e4b7e664ac4c047ff8222
imran khan <imranloveh01@gmail.com>
2023-11-27 18:18:50 +0500

    Commented out unused code

--- index.ts
+++ index.ts
@@ -2,12 +2,15 @@
 const log = console.log.bind(console, "The answer is: ");
 
 // learning type alias in typescript
-type Person={
-    name:string;
-    age:number;
-}
+// type Person={
+//     name:string;
+//     age:number;
+// }
 
-const person:Person = {
-    name:"John",
-    age:30
-}
+// const person:Person = {
+//     name:"John",
+//     age:30
+// }
+
+
+
